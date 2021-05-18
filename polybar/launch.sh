e8184faaac72aff5e371985c6db4424fa71c6a61
AgusArdizzone <agustin.ardizzone@mi.unc.edu.ar>
2021-05-18 11:53:03 -0300

    polybar configs

---
+++ polybar/launch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+polybar mybar & 
+
+my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
+if [[ $my_laptop_external_monitor = *connected* ]]; then
+    polybar hdmibar &
+fi
+
+echo "Bar launched..."
